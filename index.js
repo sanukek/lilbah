87732d813e986ce32378715c1d020a023977fb9d
rizz <ganzz808@gmail.com>
2026-04-10 23:29:49 +0700

    Add DirectMessages intent

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
   ],
 });
 
